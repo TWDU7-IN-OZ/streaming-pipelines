5f62fe101f970eb6f63331da68425af4bb843e03
Anna Christison <anastasia.c@thoughtworks.com>
2021-08-04 14:36:55 +1000

    Erinna/Anna | 20 | Adding app connection to HDFS.

--- StationValidator/src/main/scala/com/tw/apps/StationValidatorApp.scala
+++ StationValidator/src/main/scala/com/tw/apps/StationValidatorApp.scala
@@ -1,7 +1,42 @@
 package com.tw.apps
 
-class StationValidatorApp {
+import org.apache.spark.sql.SparkSession
+import org.apache.curator.framework.CuratorFrameworkFactory
+import org.apache.curator.retry.ExponentialBackoffRetry
+import org.apache.spark.sql.functions._
+
+object StationValidatorApp {
   def main(args: Array[String]): Unit = {
+
+    val retryPolicy = new ExponentialBackoffRetry(1000, 3)
+    if (args.length != 2) {
+      val message = "Two arguments are required: \"zookeeper server\" and \"application folder in zookeeper\"!"
+      throw new IllegalArgumentException(message)
+    }
+    val zookeeperConnectionString = args(0)
+
+    val zookeeperFolder = args(1)
+
+    val zkClient = CuratorFrameworkFactory.newClient(zookeeperConnectionString, retryPolicy)
+
+    zkClient.start
+
+
+
+
+
+
+    val dataLocation = new String(
+      zkClient.getData.watched.forPath(s"$zookeeperFolder/dataLocation"))
+
+    val spark = SparkSession.builder
+      .appName("StationValidatorApp")
+      .getOrCreate()
+
+
+
+    val stationDF = spark.read.csv(dataLocation)
+    print(stationDF.first())
 
   }
 }
